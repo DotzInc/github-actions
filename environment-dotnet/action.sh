15c8a56a1c43b2c8fa9343caf8eb8146818fd636
Gabriel Vieira Lavor <gabriel.lavor@dotzmkt.com.br>
2024-05-07 18:11:59 -0300

    ajuste na action

--- environment-dotnet/action.sh
+++ environment-dotnet/action.sh
@@ -17,46 +17,37 @@ else
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 fi
 
-echo $ENVIRONMENT
-echo $ENVIRONMENT_ELK
-echo $ELASTIC_APM_SERVER_URL
-echo $JWT_JWKS_PATH
-echo "$APP_NAME"
+{
+    echo ""
+    if ! [ "$ENVIRONMENT" = "dev" ]; then
+        echo ELASTIC_APM_ENABLED: \'true\'
+    fi
+    echo JKS_ALTERNATIVE_HEADER_FORWARDED_FOR: \'X-Forwarded-For\'
+    echo JWT_JWKS_PATH: \'$JWT_JWKS_PATH\'
+    echo JKS_SAFE_IPS: \'"$JKS_SAFE_IPS"\'
+    echo JKS_GOOGLE_PROJECTID: \'"$GOOGLE_PROJECTID"\'
+    echo ELASTIC_APM_SERVER_URL: \'$ELASTIC_APM_SERVER_URL\'
+    echo APPNAME: \'"$APPNAME"\'
+    echo JKS_USE_ELASTIC_APM: \'false\'
+    echo ELASTIC_APM_RECORDING: \'true\'
+    echo ELASTIC_APM_SERVICE_NAME: \'"$APPNAME"\'
+    echo ELASTIC_APM_ENVIRONMENT: \'$ENVIRONMENT_ELK\'
+    echo ELASTIC_APM_CAPTURE_BODY: \'all\'
+    echo ELASTIC_APM_CAPTURE_BODY_CONTENT_TYPES: \'*\'
+    echo ELASTIC_APM_CAPTURE_HEADERS: \'true\'
+    echo JKS_USE_APM: \'true\'
+    
+} > tmpfile
 
-# GOOGLE_PROJECTID="${{ inputs.project_id}}"
-# MAX_INSTANCES="${{ inputs.max-instances }}"
-# JKS_SAFE_IPS="${{ secrets.JKS_SAFE_IPS }}"
-# REGION="${{ inputs.gcp-region }}"
-
-# APPNAME="${{ inputs.app-name }}"
-# VPC_CONNECTOR="vpc-connector-$ENVIRONMENT"
-
-# # Agrega env
-# echo >> ./envs/env-$ENVIRONMENT.yaml
-# echo JKS_GOOGLE_PROJECTID: \'$GOOGLE_PROJECTID\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_SERVER_URL: \'$ELASTIC_APM_SERVER_URL\' >> ./envs/env-$ENVIRONMENT.yaml
-
-# if ! grep -q "JKS_SAFE_IPS:"./envs/env-$ENVIRONMENT.yaml; then
-#     echo JKS_SAFE_IPS: \'$JKS_SAFE_IPS\' >> ./envs/env-$ENVIRONMENT.yaml
-# fi
-
-# if ! [ "$ENVIRONMENT" = "dev" ]; then
-# echo ELASTIC_APM_ENABLED: \'true\' >> ./envs/env-$ENVIRONMENT.yaml
-# fi
-
-# if ! grep -q "JWT_JWKS_PATH:"./envs/env-$ENVIRONMENT.yaml; then
-# echo JWT_JWKS_PATH: \'$JWT_JWKS_PATH\' >> ./envs/env-$ENVIRONMENT.yaml
-# fi
-# if ! grep -q "JKS_ALTERNATIVE_HEADER_FORWARDED_FOR:"./envs/env-$ENVIRONMENT.yaml; then
-# echo JKS_ALTERNATIVE_HEADER_FORWARDED_FOR: \'X-Forwarded-For\' >> ./envs/env-$ENVIRONMENT.yaml
-# fi
-
-# echo APPNAME: \'$APPNAME\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo JKS_USE_ELASTIC_APM: \'false\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_RECORDING: \'true\' >> ./envs/env-$ENVIRONMENT.yaml          
-# echo ELASTIC_APM_SERVICE_NAME: \'$APPNAME\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_ENVIRONMENT: \'$ENVIRONMENT_ELK\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_CAPTURE_BODY: \'all\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_CAPTURE_BODY_CONTENT_TYPES: \'*\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo ELASTIC_APM_CAPTURE_HEADERS: \'true\' >> ./envs/env-$ENVIRONMENT.yaml
-# echo JKS_USE_APM: \'true\' >> ./envs/env-$ENVIRONMENT.yaml
+cat ./envs/env-$ENVIRONMENT.yaml tmpfile > merged_temp.yaml
+awk '
+  BEGIN { FS=": " }
+  !/^[[:space:]]*$/ && !/^#/ {
+    key = $1
+    if (!seen[key]) {
+      print
+      seen[key] = 1
+    }
+  }
+' merged_temp.yaml > ./envs/env-$ENVIRONMENT.yaml
+rm tmpfile merged_temp.yaml
