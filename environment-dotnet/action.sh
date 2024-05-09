0133787f721fab50e40b54b119ea66f302396a7d
Gabriel Vieira Lavor <gabriel.lavor@dotzmkt.com.br>
2024-05-09 10:34:31 -0300

    trazer variaveis para o projeto

--- environment-dotnet/action.sh
+++ environment-dotnet/action.sh
@@ -2,17 +2,14 @@
 
 if [ "$GITHUB_REF_NAME" = 'main' ]; then
     ENVIRONMENT='prd'
-    ENVIRONMENT_ELK='production'
     ELASTIC_APM_SERVER_URL='http://10.56.0.2:8200'
     JWT_JWKS_PATH="https://api.dotz.com.br/accounts/api/default/"
 elif [ "$GITHUB_REF_NAME" = 'staging' ]; then
     ENVIRONMENT='uat'
-    ENVIRONMENT_ELK='uat'
     ELASTIC_APM_SERVER_URL='http://10.204.0.112:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 else
     ENVIRONMENT='dev'
-    ENVIRONMENT_ELK='develop'
     ELASTIC_APM_SERVER_URL='http://10.221.0.114:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 fi
@@ -21,21 +18,12 @@ fi
     echo ""
     if ! [ "$ENVIRONMENT" = "dev" ]; then
         echo "ELASTIC_APM_ENABLED: 'true'"
-    fi
-    echo "JKS_ALTERNATIVE_HEADER_FORWARDED_FOR: 'X-Forwarded-For'"
+    fi          
     echo "JWT_JWKS_PATH: '$JWT_JWKS_PATH'"
     echo "JKS_SAFE_IPS: '$JKS_SAFE_IPS'"
     echo "JKS_GOOGLE_PROJECTID: '$GOOGLE_PROJECTID'"
     echo "ELASTIC_APM_SERVER_URL: '$ELASTIC_APM_SERVER_URL'"
-    echo "APPNAME: '$APPNAME'"
-    echo "JKS_USE_ELASTIC_APM: 'false'"
-    echo "ELASTIC_APM_RECORDING: 'true'"
-    echo "ELASTIC_APM_SERVICE_NAME: '$APPNAME'"
-    echo "ELASTIC_APM_ENVIRONMENT: '$ENVIRONMENT_ELK'"
-    echo "ELASTIC_APM_CAPTURE_BODY: 'all'"
-    echo "ELASTIC_APM_CAPTURE_BODY_CONTENT_TYPES: '*'"
-    echo "ELASTIC_APM_CAPTURE_HEADERS: 'true'"
-    echo "JKS_USE_APM: 'true'"
+          
     echo "PLATFORM: 'Cloudrun'"
     
 } > tmpfile
