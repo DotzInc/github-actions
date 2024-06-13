b164ef83c548e62aac0eb11813d5a87bfe0080c8
Gabriel Lavor <gvlavor@gmail.com>
2024-06-13 14:33:17 -0300

    add env qa (#2)
    * adicionar action para definir as variáveis de ambiente
    
    ---------
    
    Co-authored-by: Gabriel Vieira Lavor <gabriel.lavor@dotzmkt.com.br>

--- environment-dotnet/action.sh
+++ environment-dotnet/action.sh
@@ -2,15 +2,19 @@
 
 if [ "$GITHUB_REF_NAME" = 'main' ]; then
     ENVIRONMENT='prd'
-    ELASTIC_APM_SERVER_URL='http://10.56.0.2:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.noverde.com:8200'
     JWT_JWKS_PATH="https://api.dotz.com.br/accounts/api/default/"
 elif [ "$GITHUB_REF_NAME" = 'staging' ]; then
     ENVIRONMENT='uat'
-    ELASTIC_APM_SERVER_URL='http://10.204.0.112:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.uat.noverde.com:8200'
+    JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
+elif [ "$GITHUB_REF_NAME" = 'qa' ]; then
+    ENVIRONMENT='uat'
+    ELASTIC_APM_SERVER_URL='http://elastic.uat.noverde.com:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 else
     ENVIRONMENT='dev'
-    ELASTIC_APM_SERVER_URL='http://10.221.0.114:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.dev.noverde.com:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 fi
 
@@ -18,14 +22,11 @@ fi
     echo ""
     if ! [ "$ENVIRONMENT" = "dev" ]; then
         echo "ELASTIC_APM_ENABLED: 'true'"
-    fi          
+    fi
     echo "JWT_JWKS_PATH: '$JWT_JWKS_PATH'"
     echo "JKS_SAFE_IPS: '$JKS_SAFE_IPS'"
     echo "JKS_GOOGLE_PROJECTID: '$GOOGLE_PROJECTID'"
     echo "ELASTIC_APM_SERVER_URL: '$ELASTIC_APM_SERVER_URL'"
-          
-    echo "PLATFORM: 'Cloudrun'"
-    
 } > tmpfile
 
 cat ./envs/env-$ENVIRONMENT.yaml tmpfile > merged_temp.yaml
