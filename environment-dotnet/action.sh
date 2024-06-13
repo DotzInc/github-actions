93ab7891c8ef98a07592f6975276a2ac80b58eb3
Gabriel Vieira Lavor <gabriel.lavor@dotzmkt.com.br>
2024-06-13 14:27:15 -0300

    utilizar dns no lugar dos ips

--- environment-dotnet/action.sh
+++ environment-dotnet/action.sh
@@ -2,19 +2,19 @@
 
 if [ "$GITHUB_REF_NAME" = 'main' ]; then
     ENVIRONMENT='prd'
-    ELASTIC_APM_SERVER_URL='http://10.56.0.2:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.noverde.com:8200'
     JWT_JWKS_PATH="https://api.dotz.com.br/accounts/api/default/"
 elif [ "$GITHUB_REF_NAME" = 'staging' ]; then
     ENVIRONMENT='uat'
-    ELASTIC_APM_SERVER_URL='http://10.55.0.2:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.uat.noverde.com:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 elif [ "$GITHUB_REF_NAME" = 'qa' ]; then
     ENVIRONMENT='uat'
-    ELASTIC_APM_SERVER_URL='http://10.55.0.2:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.uat.noverde.com:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 else
     ENVIRONMENT='dev'
-    ELASTIC_APM_SERVER_URL='http://10.54.0.2:8200'
+    ELASTIC_APM_SERVER_URL='http://elastic.dev.noverde.com:8200'
     JWT_JWKS_PATH="https://uat.dotznext.com/accounts/api/default/"
 fi
 
@@ -27,7 +27,6 @@ fi
     echo "JKS_SAFE_IPS: '$JKS_SAFE_IPS'"
     echo "JKS_GOOGLE_PROJECTID: '$GOOGLE_PROJECTID'"
     echo "ELASTIC_APM_SERVER_URL: '$ELASTIC_APM_SERVER_URL'"
-    echo "PLATFORM: 'Cloudrun'"
 } > tmpfile
 
 cat ./envs/env-$ENVIRONMENT.yaml tmpfile > merged_temp.yaml
